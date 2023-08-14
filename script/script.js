478c0c3d99e640616bdfb27da3700dfd4ba113b1
rahulg221 <125697931+rahulg221@users.noreply.github.com>
2023-08-14 02:12:24 -0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
     var top = window.scrollY;
-    if (top >= 75) {
+    if (top >= 100) {
         header.classList.add('navbarDark');
     } else {
         header.classList.remove('navbarDark');
